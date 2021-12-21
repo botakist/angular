807c79792998bdcf8514eb87a02a51b6bf559eba
botakist <47638800+botakist@users.noreply.github.com>
2021-12-21 16:13:58 +0800

    force update to ng version 13 by creating new project with latest cli

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,35 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'github-page'`, () => {
+  it(`should have as title 'yx-ng-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('github-page');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('yx-ng-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('github-page app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('yx-ng-portfolio app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,10 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'botakist\'s angular page';
-  animationOpacity = 1;
-  animationRendered: boolean;
-  displayWidthCompatibilityMessage = false;
-  constructor() { }
-  @HostListener('window:resize')
-  checkWidth() {
-    if (window.innerWidth > 299) {
-      this.displayWidthCompatibilityMessage = true;
-    } else {
-      this.displayWidthCompatibilityMessage = false;
-    }
-  }
-  ngOnInit(): void {
-    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    // Add 'implements OnInit' to the class.
-    this.animationRendered = true;
-    const fadeEffect = setInterval(() => {
-      if (this.animationOpacity > 0) {
-        this.animationOpacity -= 0.1;
-      } else {
-        clearInterval(fadeEffect);
-        this.animationRendered = false;
-      }
-    }, 150);
-  }
+export class AppComponent {
+  title = 'yx-ng-portfolio';
 }
